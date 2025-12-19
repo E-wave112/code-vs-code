ca7e53514d2eee16b62aaf0f5b51b642f08649c4
E-wave112 <osagieiyayi09@gmail.com>
2025-12-19 14:00:49 +0100

    feat: add multiplication

--- calculator.py
+++ calculator.py
@@ -1,9 +1,14 @@
 class Calculator:
     def add(self, x, y):
         return x + y
+    
+    def multiply(self, x, y):
+        return x * y
 
 if __name__ == "__main__":
     calc = Calculator()
     print("Welcome to Calculator!")
     print("Current feature: Addition")
     print("Example: 5 + 3 =", calc.add(5, 3))
+    print("Current feature: Multiplication")
+    print("Example: 5 * 3 =", calc.multiply(5, 3))
